d1366cf76597241d299124c35856fd1d2af76ac6
Reventh-K <reventhkumar1234@gmail.com>
2024-01-15 19:56:09 +0530

    Fixing a issue on Capitalize

--- Capitalize.java
+++ Capitalize.java
@@ -4,21 +4,19 @@ public class Capitalize {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in); 
         StringBuffer sBuffer = new StringBuffer();
+        String Sresult = "";
         while (true){
             try{
                 System.out.println("Enter your sentence!");
                 String userInput = myScanner.nextLine();
-                String Ainput[]= userInput.split(" ");
-                String result[] = new String[userInput.length()];
-                String Sresult ="";
-                    for(int i =0; i<Ainput.length;i++){
-                        char toCap = Ainput[i].charAt(0);
-                        char UpperCased = Character.toUpperCase(toCap);
-                        String Replaced = Ainput[i].replace(toCap, UpperCased);
-                        result[i] = Replaced;
-                        sBuffer.append(result[i] + " ");
-                        Sresult = sBuffer.toString();
-                    }
+                String afterSplit[]= userInput.split(" ");
+                for(int i=0;i<afterSplit.length;i++){
+                    String indexzero = afterSplit[i].substring(0,1);
+                    String a = afterSplit[i].substring(0, 1).toUpperCase();
+                    String result = afterSplit[i].replaceFirst(indexzero, a);
+                    sBuffer.append(result + " ");
+                    Sresult = sBuffer.toString();
+                }
                 System.out.println(Sresult);
                 break;
             } catch(Exception e){
