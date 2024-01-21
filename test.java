dc96237e7f663c47c8dacd2bda3036d721048aca
Reventh-K <reventhkumar1234@gmail.com>
2024-01-22 00:06:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 public class test {
+    
     public static void main(String[] args) {
-        for(int i =0; i<=100; i+=5){
-
+        String output  = "";
+        for(int i =0; i<5; i++){
+            output +="*";
+            System.out.println(output);
         }
 
     }
